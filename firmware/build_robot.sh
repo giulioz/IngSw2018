91b6654a84814e9b46a7f9f103f7ff813bfabe51
giulioz <mail.zausa.giulio@gmail.com>
2018-11-18 21:43:53 +0100

    Fix arm eabi

--- firmware/build_robot.sh
+++ firmware/build_robot.sh
@@ -2,5 +2,5 @@
 
 mkdir robot_build
 cd robot_build
-CC=arm-linux-gnueabi-gcc CXX=CC=arm-linux-gnueabi-gcc cmake -DROBOT=ON -DCMAKE_TOOLCHAIN_FILE=deps/opencv/platforms/linux/arm-gnueabi.toolchain.cmake ..
+CC=arm-linux-gnueabi-gcc CXX=CC=arm-linux-gnueabi-gcc cmake -DROBOT=ON -DSOFTFP=ON -DCMAKE_TOOLCHAIN_FILE=deps/opencv/platforms/linux/arm-gnueabi.toolchain.cmake ..
 make
