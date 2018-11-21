eed63a592e192c21b6b1cc4defa0c4d378e9d347
Baccega <sandrobac@hotmail.it>
2018-11-21 10:45:15 +0100

    OnBoarding first draft

--- WatchDoge/app/src/main/java/dogedroid/com/watchdoge/MainActivity.java
+++ WatchDoge/app/src/main/java/dogedroid/com/watchdoge/MainActivity.java
@@ -1,13 +1,32 @@
 package dogedroid.com.watchdoge;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import dogedroid.com.watchdoge.onboarding.OnBoarding_1;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button onBoardBtn = findViewById(R.id.onBoardButton);
+        onBoardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                go_OnBording();
+            }
+        });
+    }
+
+    public void go_OnBording() {
+        startActivity(new Intent(this, OnBoarding_1.class));
     }
 }
---
+++ WatchDoge/app/src/main/java/dogedroid/com/watchdoge/onboarding/OnBoarding_1.java
@@ -0,0 +1,15 @@
+package dogedroid.com.watchdoge.onboarding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dogedroid.com.watchdoge.R;
+
+public class OnBoarding_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding_1);
+    }
+}
