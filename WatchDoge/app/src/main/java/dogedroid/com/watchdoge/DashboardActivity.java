c28231af25f618ef2ab3a838ab67dcb89c11f03f
Baccega <sandrobac@hotmail.it>
2018-12-06 23:37:55 +0100

    Prima Dashboard

--- WatchDoge/app/src/main/java/dogedroid/com/watchdoge/DashboardActivity.java
+++ WatchDoge/app/src/main/java/dogedroid/com/watchdoge/DashboardActivity.java
@@ -1,13 +1,25 @@
 package dogedroid.com.watchdoge;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import dogedroid.com.watchdoge.dogelog.DogeLogActivity;
+import dogedroid.com.watchdoge.dogelog.ListaLog;
+
 public class DashboardActivity extends AppCompatActivity {
-    Button liveFeedBtn;
+    Button moveLeft;
+    Button moveRight;
     TextView connectedText;
+    ImageView liveFeedImage;
+    LiveFeed liveThread;
+    RecyclerView unreadLog;
+    TextView historyText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +27,29 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         this.connectedText = findViewById(R.id.connectedText_dashboard);
+        this.moveLeft = findViewById(R.id.left_btn);
+        this.moveRight = findViewById(R.id.right_btn);
+        this.liveFeedImage = findViewById(R.id.live_image_view);
+        this.unreadLog = findViewById(R.id.unread_log_view);
+        this.historyText = findViewById(R.id.history_text);
 
-        this.liveFeedBtn = findViewById(R.id.live_activity_btn);
+        liveThread = new LiveFeed(liveFeedImage);
+        liveThread.execute();
+        
+        ListaLog listAdaptor = new ListaLog(getApplicationContext(), "/unread");
+        unreadLog.setAdapter(listAdaptor);
+        unreadLog.setLayoutManager(new LinearLayoutManager(this));
+
+        historyText.setOnClickListener((v) ->
+                startActivity(new Intent(this, DogeLogActivity.class)));
 
         this.connectedText.setText(DiscoveryActivity.dogeAddress);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        liveThread.stopLive();
     }
 }
